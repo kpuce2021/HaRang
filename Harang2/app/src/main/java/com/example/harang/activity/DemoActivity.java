5e3d3ddd747d4441c1c003a8c27644e9ed6578c5
Heejeong01110 <48792627+Heejeong01110@users.noreply.github.com>
2021-03-29 01:12:13 +0900

    s3 합친 버전

---
+++ Harang2/app/src/main/java/com/example/harang/activity/DemoActivity.java
@@ -0,0 +1,124 @@
+package com.example.harang.activity;
+
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.harang.GazeTrackerManager;
+import com.example.harang.R;
+import com.example.harang.view.GazePathView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import camp.visual.gazetracker.GazeTracker;
+import camp.visual.gazetracker.callback.GazeCallback;
+import camp.visual.gazetracker.filter.OneEuroFilterManager;
+import camp.visual.gazetracker.gaze.GazeInfo;
+import camp.visual.gazetracker.state.EyeMovementState;
+import camp.visual.gazetracker.util.ViewLayoutChecker;
+
+public class DemoActivity extends AppCompatActivity {
+  private static final String TAG = DemoActivity.class.getSimpleName();
+  private final ViewLayoutChecker viewLayoutChecker = new ViewLayoutChecker();
+  private GazePathView gazePathView;
+  private GazeTrackerManager gazeTrackerManager;
+  private final OneEuroFilterManager oneEuroFilterManager = new OneEuroFilterManager(
+      2, 30, 0.5F, 0.001F, 1.0F);
+
+  private ConcentrateManager concentrateManager;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_demo);
+    gazeTrackerManager = GazeTrackerManager.getInstance();
+    Log.i(TAG, "gazeTracker version: " + GazeTracker.getVersionName());
+
+
+    concentrateManager = ConcentrateManager.getInstance();
+    //이전 객체에서 저장했던 값이 의도대로 잘 불러와짐
+    //concentrateManager.printTest();
+
+
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    Log.i(TAG, "onStart");
+    gazeTrackerManager.setGazeTrackerCallbacks(gazeCallback);
+    initView();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    gazeTrackerManager.startGazeTracking();
+    setOffsetOfView();
+    Log.i(TAG, "onResume");
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    gazeTrackerManager.stopGazeTracking();
+    Log.i(TAG, "onPause");
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    gazeTrackerManager.removeCallbacks(gazeCallback);
+    Log.i(TAG, "onStop");
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+  }
+
+  private void initView() {
+    gazePathView = findViewById(R.id.gazePathView);
+
+    AssetManager am = getResources().getAssets();
+    InputStream is = null;
+
+    try {
+      is = am.open("palace_seoul.jpg");
+      Bitmap bm = BitmapFactory.decodeStream(is);
+      ImageView catView = findViewById(R.id.catImage);
+      catView.setImageBitmap(bm);
+      is.close();
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void setOffsetOfView() {
+    viewLayoutChecker.setOverlayView(gazePathView, new ViewLayoutChecker.ViewLayoutListener() {
+      @Override
+      public void getOffset(int x, int y) {
+        gazePathView.setOffset(x, y);
+      }
+    });
+  }
+
+  private final GazeCallback gazeCallback = new GazeCallback() {
+    @Override
+    public void onGaze(GazeInfo gazeInfo) {
+      if (oneEuroFilterManager.filterValues(gazeInfo.timestamp, gazeInfo.x, gazeInfo.y)) {
+        float[] filtered = oneEuroFilterManager.getFilteredValues();
+        gazePathView.onGaze(filtered[0], filtered[1], gazeInfo.eyeMovementState == EyeMovementState.FIXATION);
+        
+        //이 부분에서 concentrateManager 실행
+      }
+    }
+  };
+}
---
+++ Harang2/app/src/main/java/com/example/harang/activity/Fragment3.java
@@ -0,0 +1,60 @@
+package com.example.harang.activity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.harang.R;
+
+public class Fragment3 extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       view = inflater.inflate(R.layout.fragment3, container, false);
+
+       return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final Button btnLogintest = view.findViewById(R.id.setBtnLogin);
+        final Button btnOpencvTest = view.findViewById(R.id.setBtnEye);
+        final Button btnMedia = view.findViewById(R.id.setBtnMedia);
+
+        btnLogintest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(getContext(), AccountLoginActivity.class);
+                //startActivity(intent);
+            }
+        });
+
+        btnOpencvTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnMedia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MediaActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
