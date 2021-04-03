5e1c02ccbed36e15e78e20a0f526a5e3241309a7
Heejeong01110 <48792627+Heejeong01110@users.noreply.github.com>
2021-04-04 01:16:18 +0900

    강의 영상 리스트 세팅
    학생 UI용 프래그먼트 이름 앞에 stu_를 붙혀서 수정했습니다

--- Harang2/app/src/main/java/com/example/harang/activity/StudentFragment1.java
+++ Harang2/app/src/main/java/com/example/harang/activity/StudentFragment1.java
@@ -1,15 +1,18 @@
 package com.example.harang.activity;
 
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
 
@@ -26,16 +29,41 @@ public class StudentFragment1 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
        view = inflater.inflate(R.layout.stu_fragment1, container, false);
-        ListView listView = (ListView) view.findViewById(R.id.video_list);
 
-        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                menuItems
-        );
+       ListView listview;
+       VideoListViewAdapter adapter;
 
-        listView.setAdapter(listViewAdapter);
+        // Adapter 생성
+        adapter = new VideoListViewAdapter() ;
+
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) view.findViewById(R.id.video_list);
+        listview.setAdapter(adapter);
+
+        // 첫 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(getContext(), R.drawable.ic_baseline_account_box_24),
+                "Box", "Account Box Black 36dp") ;
+        // 두 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(getContext(), R.drawable.ic_baseline_account_circle_24),
+                "Circle", "Account Circle Black 36dp") ;
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(getContext(), R.drawable.ic_baseline_assignment_ind_24),
+                "Ind", "Assignment Ind Black 36dp") ;
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // get item
+                VideoListViewItem item = (VideoListViewItem) parent.getItemAtPosition(position) ;
+                
+                //클릭한 리스트의 각 항목이름 저장. --> 다른 fragment로 보내서 계속 진행
+                String titleStr = item.getTitle() ;
+                String descStr = item.getDesc() ;
+                Drawable iconDrawable = item.getIcon() ;
+            }
+        });
 
        return view;
     }
+
 }
