48678500aff982a67e2567a04cb212b2475f1b8d
Minu <rlaalsdn160@naver.com>
2021-05-11 00:13:06 +0900

    .
    .

---
+++ Harang3/FFmpegAndroid64/build/generated/source/buildConfig/debug/com/github/hiteshsondhi88/libffmpeg/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.github.hiteshsondhi88.libffmpeg;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.github.hiteshsondhi88.libffmpeg";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.github.hiteshsondhi88.libffmpeg";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 27;
+  public static final String VERSION_NAME = "0.3.2";
+}
